06c0d37547ec61bd197248a82543f837c5688e74
Dan Kahle <dan.kahle@gmail.com>
2017-11-03 08:04:07 -0700

    latest

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 
 module.exports = {
     Validate: require('./lib/validate'),
-    BasicError: require('./lib/errors/basicError'),
-    ExtendedError: require('./lib/errors/extendedError'),
+    errors: {
+      BasicError: require('./lib/errors/basicError'),
+      ExtendedError: require('./lib/errors/extendedError')
+    },
     middleware: {
         errorHandler: require('./lib/middleware/errorHandler'),
         notFound: require('./lib/middleware/notFound')

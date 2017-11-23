d74f3868bb8e25af51e4b9c924bc510a051309d5
Dan Kahle <dan.kahle@gmail.com>
2017-11-22 18:09:04 -0800

    made config be area specific

--- play.js
+++ play.js
@@ -1,4 +1,17 @@
 
+
+const _ = require('lodash');
+
+let obj = {name:'dank'};
+console.log(_.merge(obj, {age:50}), obj);
+
+
+
+
+
+
+
+/*
 const eql = require('deep-eql');
 
 
@@ -6,6 +19,7 @@ let a = {name: 'dank', age: 50};
 let b = {age: 50, name: 'dank'};
 
 console.log(eql(5,'5'))
+*/
 
 
 
